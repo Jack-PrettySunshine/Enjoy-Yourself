143de5656daeea83c320caf979db3f23b9034c22
若尘 <Lee981265@gmail.com>
2019-12-10 17:22:44 +0800

    Update ADD.vue

--- Jack's Note/ADD.vue
+++ Jack's Note/ADD.vue
@@ -4,7 +4,7 @@
             v-model="isShow"
             width="620"
             :closable="true"
-            :mask-closable="true"
+            :mask-closable="false"
             :styles="styles"
             @on-close="close(false)"
             class-name="cover"
@@ -15,8 +15,8 @@
                     <FormItem label="部门名称" prop="deptName" >
                         <Input v-model="formValidate.deptName" placeholder="请输入部门名称" class="input-width" />
                     </FormItem>
-                     <FormItem v-if="formValidate.parentCode != 0 || currentType === 'Add'" label="上级部门" prop="parentCode" >
-                        <Select filterable v-model="formValidate.parentCode" placeholder="请选择上级部门" class="input-width">
+                     <FormItem v-if="isEmpty && (formValidate.parentCode != 0 || currentType === 'Add')" label="上级部门" prop="parentCode" >
+                        <Select v-if="rendering" filterable v-model="formValidate.parentCode" placeholder="请选择上级部门" class="input-width">
                             <Option v-for="item in DataList" :key="item.id"  :value="item.deptCode">{{item.deptName}}</Option>
                         </Select>
                     </FormItem>
@@ -24,7 +24,7 @@
                         <Input v-model="formValidate.sort" placeholder="请输入部门排序" class="input-width" />
                     </FormItem>
                     <FormItem label="部门编制" prop="authorized" >
-                        <Input v-model="formValidate.authorized" placeholder="请输入部门编制" class="input-width" /> 人
+                        <Input v-model="formValidate.authorized" placeholder="请输入部门编制" class="input-width" /> <span>人</span>
                     </FormItem>
                     <FormItem label="部门描述" prop="desc" >
                         <Input type="textarea" :rows="4"  v-model="formValidate.desc" placeholder="请输入部门描述" class="input-width" />
@@ -40,20 +40,23 @@
             <Button v-if="currentType === 'Edit'" @click="Submit"  type="primary">确定</Button>
         </div>
          <Spin size="large" fix v-if="spinShow"></Spin>
-        </Drawer>    
+        </Drawer>
 </template>
 <script>
-    import ajaxUrl from "_s/ajaxUrl"; 
+    import ajaxUrl from "_s/ajaxUrl";
     import { getChooseCompanyId } from "@/utils/storages/companyStorage";
     export default {
         name: "AddDepartment",
-        props: {
-            currentType:{
-                currentType: String
-            },
-        },
+        // props: {
+        //     currentType:{
+        //         type: String
+        //     },
+        // },
         data () {
             return {
+                currentType: '',
+                rendering: false,
+                isEmpty: true,
                 isShow: false,
                 spinShow: false,
                 companyCode: getChooseCompanyId(),
@@ -83,45 +86,41 @@
                     deptName: [
                         { required: true, message: '部门名称不能为空', trigger: 'blur' },
                         { validator:(rule, value, cb)=>{this.deptNamelenValid(value, 16, cb)}, trigger: 'change'}
-            
                     ],
                     sort: [
-                        { 
-                            type: 'number',
-                            message: '请输入数字',
-                            transform(value){
-                               return Number(value)
-                            }
+                        {
+                            type: 'string',
+                            pattern: /^[1-9]\d{0,1}[0]*$/,
+                            message: '请输入1-100以内正整数',
+                            trigger: 'change',
+                            max: 3
                         },
                         { validator: (rule, value, cb ) =>{ this.checkNum(value, value, cb , 'sort' ) } , trigger: 'change'} ,
-                        
                     ],
                     authorized: [
-                        { 
-                            type: 'number',
-                            message: '请输入数字',
-                            transform(value){
-                               return Number(value)
-                            }
-                        },
-                        { validator: (rule, value, cb ) =>{ this.checkNum(value, value, cb , 'authorized' ) } , trigger: 'change'} 
+                        {
+                            required: true,
+                            type: 'string',
+                            pattern: /^([1-9]\d{0,4}|100000)$/,
+                            message: '请输入1-100000以内正整数',
+                            trigger: 'change',
+                        }
                     ],
                     parentCode: [
                         { required: true, message: '上级部门不能为空', trigger: 'blur' },
                     ],
                     desc: [
                         { type: 'string', message: '最多140个字符', trigger: 'change' , max: 140},
-                        // { validator: (rule, value, cb ) =>{ this.checkdesc(value, value, cb , 'authorized' ) } , trigger: 'change'} 
                     ],
-                   
+
                 }
             }
         },
         created(){
-            
+
         },
         mounted(){
-            
+
         },
         destroyed(){
         },
@@ -130,34 +129,36 @@
                 this.formValidate = {
                     deptName: this.currentdeptName, // 部门名称
                     parentCode: this.currentparentCode, // 上级部门编码
-                    sort: this.currentsort, // 部门排序
+                    sort: this.currentsort ? this.currentsort.toString() : '', // 部门排序
                     desc: this.currentdesc, // 部门描述
-                    authorized: this.currentauthorized, // 部门编制
+                    authorized: this.currentauthorized ? this.currentauthorized.toString() : '', // 部门编制
                 }
             },
             Submit: function(){
-                this.$refs.formValidate.validate((valid) => { 
+                this.$refs.formValidate.validate((valid) => {
                     if (valid) {
                        // 调用接口
                        const { formValidate } = this ;
                        this.spinShow = true;
                        this.$post({
                             url:ajaxUrl.deptment.updateDeptInfo,
+                            notShowError: true,
                             method: 'post',
-                            data: { 
+                            data: {
                                 id: this.currentId ,
                                 companyCode: this.companyCode ,
                                 deptName: formValidate.deptName,
                                 parentCode: formValidate.parentCode,
                                 sort: formValidate.sort,
                                 authorized: formValidate.authorized,
-                                desc: formValidate.desc,
+                                deptDesc: formValidate.desc,
+                                deptCode:this.currentDeptcode,
                             }
-                        }).then(res => { 
-                            if ( res.status == 200 ){ 
+                        }).then(res => {
+                            if ( res.status == 200 ){
                                 this.spinShow = false;
                                 this.handleReset('formValidate');
-                                this.$Message.success('操作成功');
+                                this.$Message.success('保存成功');
                                 this.close(false);
                                 this.$emit('onreloadData');
                             }
@@ -166,24 +167,35 @@
                             this.$Message.error(err.msg);
                         });
                         } else {
-                            this.$Message.error('请检查表单!');
+                            // this.$Message.error('请检查表单!');
                         }
                 })
             },
-            getDeptList: function(){
+            getDeptList: function(){ 
+                let deptCode = '';
+                if (this.currentType === 'Add'){ 
+                    deptCode = '';
+                    console.log('AddPROPS:','Add');
+                }else{
+                    deptCode = this.currentDeptcode;
+                }
+                this.spinShow = true;
                 this.$post({
                     url:ajaxUrl.deptment.queryDeptSelectList,
                     method: 'post',
-                    data: { 
+                    data: {
                         companyCode: this.companyCode ,
-                        deptCode: '',
+                        deptCode: deptCode,
                     }
-                }).then(res => { 
+                }).then(res => {
+                    this.spinShow = false;
                     if ( res.status == 200 ){
                         this.DataList = res.data;
+                        this.rendering = true;
                     }
                 }).catch(err => {
-                    this.$Message.error(err.msg);
+                    this.spinShow = false;
+                    // this.$Message.error(err.msg);
                 });
             },
             checkdesc: function( str, num, cb  ){
@@ -200,13 +212,20 @@
                     if(str && str%1 != 0 ){
                        return cb(new Error('请输入整数'))
                     }
+
+                    if(str.toString().length>6){
+                        return cb(new Error('请输入6位以内的整数'))
+                    }
                 }
-               
+
                 cb()
             },
             deptNamelenValid: function( str, num, cb ){
                 if(str){
-                    let len = str.length;
+                    let len = str.trim().length;
+                    if(len == 0){
+                        return cb(new Error('部门名称不能为空'))
+                    }
                     if(len > num){
                         return cb(new Error('最多16个字符'))
                     }
@@ -223,28 +242,30 @@
                     this.handleReset('formValidate');
                 }
             },
-            handleSubmit (name) { 
+            handleSubmit (name) {
                 this.$refs[name].validate((valid) => { 
                     if (valid) {
                        // 调用接口
                        const { formValidate } = this ;
                        this.spinShow = true;
+                       console.log(formValidate.parentCode)
                        this.$post({
                             url:ajaxUrl.deptment.updateDeptInfo,
                             method: 'post',
-                            data: { 
+                            notShowError: true,
+                            data: {
                                 companyCode: this.companyCode ,
                                 deptName: formValidate.deptName,
-                                parentCode: formValidate.parentCode,
+                                parentCode: formValidate.parentCode?formValidate.parentCode : '',
                                 sort: formValidate.sort,
                                 authorized: formValidate.authorized,
-                                desc: formValidate.desc,
+                                deptDesc: formValidate.desc,
                             }
-                        }).then(res => { 
-                            if ( res.status == 200 ){ 
+                        }).then(res => {
+                            if ( res.status == 200 ){
                                 this.spinShow = false;
                                 this.handleReset('formValidate');
-                                this.$Message.success('操作成功');
+                                this.$Message.success('保存成功');
                                 this.close(false);
                                 this.$emit('onreloadData');
                             }
@@ -253,7 +274,7 @@
                             this.$Message.error(err.msg);
                         });
                         } else {
-                            this.$Message.error('请检查表单!');
+                            // this.$Message.error('请检查表单!');
                         }
                 })
             },
@@ -265,11 +286,11 @@
                 this.$post({
                     url:ajaxUrl.deptment.queryDepartmentByCode+`/`+this.currentDeptcode,
                     method: 'post',
-                    data: { 
-                       
+                    data: {
+
                     }
-                }).then(res => { 
-                    if ( res.status == 200 ){
+                }).then(res => {
+                    if ( res.status == 200 ){  console.log(res)
                          this.spinShow = false;
                         // 赋值
                         this.currentDeptcode = res.data.deptCode;
@@ -283,25 +304,25 @@
                         this.formValidate = {
                             deptName: res.data.deptName, // 部门名称
                             parentCode:  res.data.parentCode, // 上级部门编码
-                            sort:  res.data.sort, // 部门排序
+                            sort:  res.data.sort ? res.data.sort.toString() : '', // 部门排序
                             desc: res.data.deptDesc, // 部门描述
-                            authorized: res.data.authorized, // 部门编制
+                            authorized: res.data.authorized ? res.data.authorized.toString() : '', // 部门编制
                         }
                     }
                 }).catch(err => {
                     this.spinShow = false;
-                    this.$Message.error(err.msg);
+                    // this.$Message.error(err.msg);
                 });
             },
         },
-      
+
     }
 </script>
 <style lang="less">
 
 
 .l-content{
-    padding: 55px;
+    padding: 50px;
     font-size: 14px;
     .ivu-form .ivu-form-item-label{
         padding: 12px 12px 10px 0;
@@ -327,8 +348,8 @@
             width: 390px;
         }
     }
-   
-    
+
+
 }
  .L-demo-drawer-footer{
         width: 100%;
